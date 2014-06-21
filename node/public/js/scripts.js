3bed2382ecc94b03b9c9164479ec7dd1a84f2395
poofytoo <victorhung92@gmail.com>
2014-06-21 02:17:55 -0400

    strikes and time starting

--- node/public/js/scripts.js
+++ node/public/js/scripts.js
@@ -15,18 +15,27 @@ $(document).ready(function(){
     });
   });
   
+  $(document).on('click', '.strike', function(){
+    var id = $(this).attr('class').split(' ')[1];
+    var url = '/strike';
+    var data = {songId: id};
+    $.post(url, data, function(e) {
+      console.log('strike for song');
+    });
+  });
+  
   dataRef.on('value', function(snapshot) {
     var queue = snapshot.val().queue;
     var html = '';
     var counter = 0;
     var playing = '&#9658;';
     for (i in queue) {
-      
+      var id = i;
       var video = queue[i];
       html += '<div class="playlist-item">';
       html += '<div class="isplaying">' + playing + '</div> ';
       html += '<div class="uploader">' + video.owner + '</div> ';
-      html += '<div class="title">' + video.name + '</div> ';
+      html += '<div class="title">' + video.name + '<span class="strike ' + id + '">nope</span></div> ';
       html += '</div>';
       playing = '';
       
@@ -36,6 +45,10 @@ $(document).ready(function(){
       if (playingVideo == '' || (counter == 0 && playingVideoLink != video.link)) {
         playingVideoLink = video.link;
         playingVideo = video.link + '?autoplay=1';
+        var url = '/time';
+        $.get(url function(data){
+          console.log(data);
+        })
         $('#ytplayer').attr('src',playingVideo);
       }
       
